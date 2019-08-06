b1dc729aa88515a36a91bd7a238deb3343647ab4
Rupesh Dabbir <d.rupeshkumar@gmail.com>
2019-08-05 23:33:12 -0700

    First commit

---
+++ app/controllers/index.js
@@ -0,0 +1,24 @@
+import { computed, observer } from '@ember/object';
+import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
+
+
+export default Controller.extend({
+  isDisabled: true,
+  headerTitle: 'Coming Soon',
+  responseMessage: '',
+  emailAddress: '',
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+
+  actions: {
+    saveInvitation () {
+        alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+        this.set('emailAddress', '');
+    }
+  }
+
+
+
+});
